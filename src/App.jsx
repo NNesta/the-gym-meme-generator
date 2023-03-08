c5003590f7c4cd5ed885d100431e91e095ba4b53
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-08 16:47:53 +0200

    feat:fetching data from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
-import memesData from "./memesData.js";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [meme, setMeme] = useState(
-    memesData.data.memes[
-      Math.floor(Math.random() * memesData.data.memes.length)
-    ]
-  );
+  const [memesData, setMemesData] = useState(null);
+  const [meme, setMeme] = useState("");
   const [first, setFirst] = useState("");
   const [second, setSecond] = useState("");
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((data) => {
+        setMemesData(data);
+      });
+    getRandomImage();
+  }, []);
+  const getRandomImage = () => {
+    const image =
+      memesData &&
+      memesData.data.memes[
+        Math.floor(Math.random() * memesData.data.memes.length)
+      ];
+    setMeme(image);
+  };
+  useEffect(() => {
+    getRandomImage();
+  }, [memesData]);
+
   return (
-    <main className="max-w-[550px] mx-auto my-4">
+    <main className="max-w-[550px] mx-auto max-h-screen ">
       <nav
         className="flex items-center justify-between w-full text-white px-9 h-16"
         style={{
@@ -22,17 +38,17 @@ function App() {
           React Course - Project 3
         </h4>
       </nav>
-      <div className="flex flex-col items-center m-4 gap-4">
+      <div className="flex flex-col items-center  m-4 gap-4">
         <div className="flex w-full gap-4 py-4 justify-center">
           <input
             onChange={(e) => setFirst(e.target.value)}
-            className="px-2 w-[50%] h-9 border border-[#D5D4D8] rounded-[5px]"
+            className="px-2 w-[50%] h-9 border border-[#D5D4D8] rounded-[5px] focus:outline-none focus:border-4 focus:border-sky-400"
             placeholder=""
             value={first}
           />
           <input
             onChange={(e) => setSecond(e.target.value)}
-            className="px-2 w-[50%] h-9 border border-[#D5D4D8] rounded-[5px]"
+            className="px-2 w-[50%] h-9 border border-[#D5D4D8] rounded-[5px]  focus:outline-none focus:border-4 focus:border-sky-400"
             value={second}
           />
         </div>
@@ -41,23 +57,25 @@ function App() {
             background:
               "linear-gradient(90.41deg, #711F8D 1.14%, #A818DA 100%)",
           }}
-          className="w-full h-10 rounded-[5px]"
+          className="w-full h-10 rounded-[5px] text-xl"
           onClick={() => {
-            setMeme(
-              memesData.data.memes[
-                Math.floor(Math.random() * memesData.data.memes.length)
-              ]
-            );
-            setFirst("");
-            setSecond("");
+            getRandomImage();
           }}
         >
           Get a new meme image 🖼
         </button>
-        <div className={`relative text-4xl font-bold text-white`}>
-          <p className="absolute top-4 inset-x-4 mx-auto text-auto">{first}</p>
-          <img className="w-full object-cover" src={meme?.url} alt="" />
-          <p className="absolute bottom-4 inset-x-4 mx-auto text-auto">
+        <div
+          className={`relative text-4xl font-bold h-[500px]  p-4 text-white`}
+        >
+          <p className="absolute top-4 inset-x-4 mx-auto text-center  text-auto">
+            {first}
+          </p>
+          <img
+            className="w-full object-contain h-full"
+            src={meme?.url}
+            alt=""
+          />
+          <p className="absolute bottom-4 inset-x-4 mx-auto text-auto text-center">
             {second}
           </p>
         </div>
